781288a50ad4dd5bf770c4cccad2fef5d7a8cf7f
Elisabeth Malek <elisabeth.malek@yahoo.fr>
2022-09-13 17:05:57 +0200

    Modification du ficher js

--- script.js
+++ script.js
@@ -2,4 +2,13 @@ console.log("ok");
 
 $(".handle").on("click", function () {
   $("nav ul").toggleClass("showing");
+  if ($("nav ul").hasClass("showing")) {
+    $("h1").hide();
+    $("i").removeClass("fa-solid fa-bars");
+    $("i").addClass("fa-solid fa-xmark");
+  } else {
+    $("h1").show();
+    $("i").removeClass("fa-solid fa-xmark");
+    $("i").addClass("fa-solid fa-bars");
+  }
 });
